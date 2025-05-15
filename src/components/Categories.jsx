8264617117b14816fbb4293e999682f5b4166da3
Zakrieva <zakrieva34t@mail.ru>
2025-05-16 01:21:47 +0300

    add: popup

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -8,7 +8,10 @@ function Categories() {
     <div class="categories">
       <ul>
         {categories.map((value, i) => (
-          <li onClick={() => setActiveIndex(i)} className={activeIndex === i ? 'active' : ''}>
+          <li
+            key={i}
+            onClick={() => setActiveIndex(i)}
+            className={activeIndex === i ? 'active' : ''}>
             {value}
           </li>
         ))}
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,6 +1,7 @@
-
-function Sort (){
-    return(
+import React from 'react';
+function Sort() {
+  const [open, setOpen] = React.useState(false);
+  return (
     <div class="sort">
       <div class="sort__label">
         <svg
@@ -8,23 +9,27 @@ function Sort (){
           height="6"
           viewBox="0 0 10 6"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>популярности</span>
       </div>
-      <div class="sort__popup">
-        <ul>
-          <li class="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
-    </div>)
-  }
-  export default Sort;
+      {open ? (
+        <div class="sort__popup">
+          <ul>
+            <li class="active">популярности</li>
+            <li>цене</li>
+            <li>алфавиту</li>
+          </ul>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+}
+export default Sort;
